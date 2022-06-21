5c52ae69bb83d3def1e3c20b0fccc17cfb25f29e
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-06-21 21:59:36 +0300

    feat: Adding comments markup.

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -11,18 +11,29 @@ function CommentList({currentUserId}) {
     // Use stats gets an empty array by default because our comment list is blank
     const [backendComments, setBackendComments] = useState([]);
     
-    console.log("backendComments", backendComments);
+    // Root comments are those who have the parrent id equal to null
+    const rootComments = backendComments.filter(
+        (backendComment) => backendComment.parentId === null
+    );
+
+    console.log('backendComments', backendComments);
 
     // We use useEffect because, we want to fetch data
     useEffect(() => {
-        getCommentsApi().then(data => {
+        getCommentsApi().then((data) => {
             setBackendComments(data);
         }); // The api is returning a promise
     }, []); // We pass empty array this means that will be trigered only once after monting the compionent
 
     // Return a comment list
     return (
-        <div className="commentList">
+        <div className="comment-list">
+            <div className="list-title">Comments</div>
+            <div className="list-container">
+                {rootComments.map((rootComment) => (
+                    <div key={rootComment.id}>{rootComment.body}</div> // We should have a unique key
+                ))}
+            </div>
         </div>
     );
 }
